e970a053e88e58690ff269321d06953cf81d83dd
renewss <2ibragimov@gmail.com>
2020-12-02 20:44:50 +0500

    disable navbar animation effect

--- src/pages/dashboard/components/Sidebar.jsx
+++ src/pages/dashboard/components/Sidebar.jsx
@@ -34,16 +34,16 @@ const useStyles = makeStyles((theme) => ({
     },
     drawerOpen: {
         width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
+        // transition: theme.transitions.create('width', {
+        //     easing: theme.transitions.easing.sharp,
+        //     duration: theme.transitions.duration.enteringScreen,
+        // }),
     },
     drawerClose: {
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
+        // transition: theme.transitions.create('width', {
+        //     easing: theme.transitions.easing.sharp,
+        //     duration: theme.transitions.duration.leavingScreen,
+        // }),
         overflowX: 'hidden',
         width: theme.spacing(7) + 1,
         [theme.breakpoints.up('sm')]: {
